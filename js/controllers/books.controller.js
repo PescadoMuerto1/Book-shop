21a9c3c106bc2d96b0994c50c007e3a661fc8776
PescadoMuerto1 <mbl0502727793@egmail.com>
2024-02-15 16:07:27 +0200

    add basic render

--- js/controllers/books.controller.js
+++ js/controllers/books.controller.js
@@ -3,3 +3,23 @@
 function onInit(){
     renderBooks()
 }
+
+function renderBooks() {
+    const books = getBooks()
+    const strHTMLs = books.map(book => `
+        <tr>
+        <td class="book-title">${book.title}</td>
+        <td class="book-price">${book.price}</td>
+        <td class="btns">
+            <button class="Read-btn">Read</button>
+            <button class="Update-btn">Update</button>
+            <button class="Delete-btn">Delete</button>
+        </td>
+        </tr>
+    `)
+
+    const elTable = document.querySelector('tbody')
+    elTable.innerHTML = strHTMLs.join('')
+
+    // renderStats()
+}
--- js/services/books.service.js
+++ js/services/books.service.js
@@ -1,17 +1,17 @@
 'use strict'
 
 var gBooks =[ 
-{id: bg4J78,
+{id: 'bg4J78',
     title: 'The adventures of Lori Ipsi',
     price: 120,
     imgUrl: 'lori-ipsi.jpg'
 },
-{id: bg4J44,
+{id: 'bg4J44',
     title: 'World atlas',
     price: 200,
     imgUrl: 'lori-ipsi.jpg'
-}
-]
+}]
+
 function getBooks(){
     return gBooks
 } 
